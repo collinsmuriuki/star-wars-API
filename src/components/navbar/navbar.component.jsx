8def2a8ac4d110ff0cb0404e26dcf8f8645b3006
Collins Muriuki <murerwacollins@gmail.com>
2019-12-28 11:23:39 +0300

    Styling navbar component

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Menu, Container } from 'semantic-ui-react';
+import './navbar.styles.css';
 
 export const Navbar = props => (
-    <Menu inverted>
+    <Menu className="navbar" inverted>
         <Container>
             <Menu.Item name="star wars api"></Menu.Item>
             <Menu.Item name="people"></Menu.Item>
