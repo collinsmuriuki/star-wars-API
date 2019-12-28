82ceae2cd944d72dd96bdbc79a8d859fd63a922a
Collins Muriuki <murerwacollins@gmail.com>
2019-12-28 11:59:05 +0300

    Implement loaders

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container, Dimmer, Loader } from 'semantic-ui-react';
 
 
 import { Navbar } from './components/navbar/navbar.component';
@@ -20,12 +20,14 @@ const App = () => {
       let result = await fetch('https://swapi.co/api/people/');
       let data = await result.json();
       setPeople(data.results);
+      setLoading(false)
     }
 
     async function fetchPlanets() {
       let result = await fetch('https://swapi.co/api/planets/');
       let data = await result.json();
       setPlanets(data.results);
+      setLoading(false)
     }
 
     fetchPeople();
@@ -37,7 +39,12 @@ const App = () => {
       <Router>
         <Navbar />
         <Container>
-          <Switch>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ): (
+            <Switch>
             <Route exact path='/'>
               <Home />
             </Route>
@@ -48,6 +55,7 @@ const App = () => {
               <Planets />
             </Route>
           </Switch>
+          )}
         </Container>
       </Router>
     </>
