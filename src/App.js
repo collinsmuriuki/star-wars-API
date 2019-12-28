faa2265e1ffd6c926f39f76d9dc6547506484920
Collins Muriuki <murerwacollins@gmail.com>
2019-12-28 11:17:07 +0300

    Make API request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable no-useless-constructor */
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar } from './components/navbar/navbar.component';
+
+const App = () => {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState([true])
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let result = await fetch('https://swapi.co/api/people/');
+      let data = await result.json();
+      setPeople(data.results);
+    }
+
+    async function fetchPlanets() {
+      let result = await fetch('https://swapi.co/api/planets/');
+      let data = await result.json();
+      setPlanets(data.results);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+  }, [])
+    
+  return(
+    <h1>Hi</h1>
+  )
+
 }
 
 export default App;
