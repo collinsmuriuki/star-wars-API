6f2e6180cd5dc231b89cc78a6fc3fe8f3a1a4afb
Collins Muriuki <murerwacollins@gmail.com>
2019-12-28 12:11:58 +0300

    Complete the people component

--- src/components/people/people.component.jsx
+++ src/components/people/people.component.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Card, Grid } from 'semantic-ui-react';
 
-export const People = props => (
-    <div>
-        <p>People</p>
-    </div>
+export const People = ({data}) => (
+    <>
+        <h1>People</h1>
+        <Grid columns={3}>
+            {data.map((people, i) => (
+            <Grid.Column key={i}>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>{people.name}</Card.Header>
+                        <Card.Description>
+                            <strong>Height</strong>
+                            <p>{people.height}</p>
+                            <strong>Mass</strong>
+                            <p>{people.mass}</p>
+                            <strong>Hair Color</strong>
+                            <p>{people.hair_color}</p>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+            </Grid.Column>
+            ))}
+        </Grid>
+    </>
 )
