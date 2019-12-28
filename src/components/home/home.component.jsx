b90133219f6da425a27e659eb7d228863288553d
Collins Muriuki <murerwacollins@gmail.com>
2019-12-28 13:50:23 +0300

    Complete home component

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export const Home = props =>  {
     return (
-        <div>
-            
+        <div className="intro">
+          <h1>Star Wars API</h1>
+          <p>
+            This is just a simple ReactJS project consuming the Star Wars API; 
+            showing details of characters, planets and starships from the Star Wars Universe.
+          </p>
+          <p>
+            The fundamentals of using react-router and react semantic-ui are covered here.
+          </p>
+          <p>
+            Visit the api documentation <a href="https://swapi.co/" target="_blank">here</a>
+          </p>
         </div>
     )
 }
